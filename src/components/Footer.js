06c9e8070919de7986ec37a15474feca880b3691
Shabarish01 <135974848+Shabarish01@users.noreply.github.com>
2023-08-21 17:26:18 +0200

    Importing Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../components/img/footer_logo.png"
 import { FaFacebook, FaTwitter, FaInstagram, FaPinterest, FaYoutube } from "react-icons/fa";
 import "../CSS/footer.css";
